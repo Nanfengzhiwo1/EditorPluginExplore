e69565fc90ad3a99c93a4c66ff783e7f9c390e8c
BaiPiaoGuai <158132087@qq.com>
2024-02-05 22:16:25 +0800

    Create a Function named DuplicateAssets

--- Plugins/Master_A/Source/Master_A/Private/AssetAction/QuickAction.cpp
+++ Plugins/Master_A/Source/Master_A/Private/AssetAction/QuickAction.cpp
@@ -16,13 +16,31 @@ void UQuickAction::DuplicateAssets(int32 NumOfDuplicates)
 	}
 
 	TArray<FAssetData> SelectedAssetsData = UEditorUtilityLibrary::GetSelectedAssetData();
+
+	// track how many assets have been duplicated
 	uint32 Counter = 0;
 
 	for (const FAssetData& SelectedAssetData : SelectedAssetsData)
 	{
 		for (int32 i=0;i<NumOfDuplicates;i++)
 		{
-		
+			const FString SourceAssetPath = SelectedAssetData.ObjectPath.ToString();
+			const FString NewDuplicatedAssetName = SelectedAssetData.AssetName.ToString() + TEXT("_") + FString::FromInt(i+1);
+			const FString NewPathName =FPaths::Combine(SelectedAssetData.PackagePath.ToString(),NewDuplicatedAssetName);
+
+			// when pointer is valid ,duplicated an asset successfully
+			if (UEditorAssetLibrary::DuplicateAsset(SourceAssetPath, NewPathName))
+			{
+				// OnlyIfIsDirty:false,means save assets no matter what 
+				UEditorAssetLibrary::SaveAsset(NewPathName,false);
+				++Counter;
+			}
 		}
 	}
+
+	if (Counter > 0)
+	{
+		ScreenPrint(TEXT("Successfully duplicated " + FString::FromInt(Counter) + "Assets"),FColor::Green );
+	}
+	
 }
